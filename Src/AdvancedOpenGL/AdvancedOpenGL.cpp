17a0c00345892a0046de9631e19c9249f8b93b7d
msychalice <matthew.ma.groupt@gmail.com>
2018-12-11 19:57:21 +0800

    渲染到texture时忘记enable depth test了, 导致物体深度不对

--- Src/AdvancedOpenGL/AdvancedOpenGL.cpp
+++ Src/AdvancedOpenGL/AdvancedOpenGL.cpp
@@ -362,6 +362,7 @@ int main()
 		// rendering commands here
 		//
 		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		glEnable(GL_DEPTH_TEST);
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
